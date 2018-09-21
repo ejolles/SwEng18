a369d6cc437c429200e72305c6fa41c5f180349c
Robin G.F. Mamie <robin.mamie@gmail.com>
2018-09-21 19:35:47 +0200

    Add GreetingActivity

---
+++ app/src/main/java/ch/epfl/sweng/bootcamp/GreetingActivity.java
@@ -0,0 +1,24 @@
+package ch.epfl.sweng.bootcamp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GreetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_greeting);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        message = "Hello " + message + "!";
+
+        // Capture the layout's TextView and set the string as its text
+        TextView greetingMessage = findViewById(R.id.greetingMessage);
+        greetingMessage.setText(message);
+    }
+}
